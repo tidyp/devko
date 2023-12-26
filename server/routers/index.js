9fa02c76032ac4015e528828c88f593537982245
6worry <dudfhr217@naver.com>
2023-12-26 15:10:48 +0900

    1차 커밋(로그인구현까지 완료)

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 
-// app.use(require('./login.route'));
 app.use("/api/googleAuth", require('./user/googleAuth'))
 app.use("/api/naverAuth", require('./user/naverAuth'))
 
