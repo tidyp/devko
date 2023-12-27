0e139574cf3b46a0b7ba5adba08b0c78ee5f53c3
6worry <dudfhr217@naver.com>
2023-12-27 16:57:31 +0900

    1차 커밋

--- server/test.sql
+++ server/test.sql
@@ -1,1 +1,21 @@
-SELECT * FROM devko.users;
+-- DROP TABLE devko.users;
+
+-- CREATE DATABASE IF NOT EXISTS devko;
+
+-- CREATE TABLE IF NOT EXISTS Users (
+--      id VARCHAR(64),
+--      userName VARCHAR(45),
+--      googleId VARCHAR(45),
+--      googleEmail VARCHAR(45),
+--      naverId VARCHAR(45),
+--      naverEmail VARCHAR(45),
+--      profileImage BLOB,
+--      workPosition VARCHAR(64),
+--      interestArea VARCHAR(64),
+--      selfDescription TEXT,
+--      createdAt DATETIME,
+--      updatedAt DATETIME,
+--      grade INTEGER,
+--      notification TINYINT(1)
+-- );
+SELECT * FROM devko.users
