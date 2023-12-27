98eb129f96dfd2054a1cb4743ebf0f13283c511d
6worry <dudfhr217@naver.com>
2023-12-27 16:58:38 +0900

    merge 충돌 병합 수정

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const app = express();
 
-<<<<<<< HEAD
-app.use("/api/googleAuth", require('./user/googleAuth'))
-app.use("/api/naverAuth", require('./user/naverAuth'))
-=======
 app.use('/api/googleAuth', require('./user/googleAuth'))
 app.use('/api/naverAuth', require('./user/naverAuth'))
 app.use('/api/additionalInfo', require('./user/additionalInfo'))
->>>>>>> main
 
 module.exports = app;
