006fa82c102e419dbe55f917fba2eced400317db
6worry <dudfhr217@naver.com>
2023-12-28 14:00:58 +0900

    틀 설정

--- server/routers/user/additionalInfo.js
+++ server/routers/user/additionalInfo.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const db = require("../../config/db")
+const db = require("../../config/db");
 const router = express.Router();
-const path = require('path')
+const path = require('path');
 
 router.get('/step2', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'public', 'userInfo.html'));
-})
+});
+
+router.get('/step3', (req, res) => {
+
+});
 
 module.exports = router;
