2064717ea679c6ac762269f22502fb1251518f50
6worry <dudfhr217@naver.com>
2024-01-02 17:12:03 +0900

    회원 정보추가탭 완료

--- server/routers/user/additionalInfo.js
+++ server/routers/user/additionalInfo.js
@@ -10,36 +10,33 @@ router.get('/step2', (req, res) => {
 
 router.post('/step3', async (req, res) => {
     const id = uuidv4();
-    const googleId = req.body.googleId
-    const userName = req.body.userName
-    const workPosition = req.body.workPosition
-    const interestArea = req.body.interestArea
-    const selfDescription = req.body.selfDescription
-    const createdAt = new Date().getTime();
-    const updatedAt = new Date().getTime();
-    // const grade = req.body.grade
-    // const notification = req.query.notification
-    try {
-        console.log(googleId)
-        console.log(req.body)
-        console.log(createdAt)
-        console.log(updatedAt)
+    const googleId = req.session.googleId;
+    const userName = req.body.userName;
+    const workPosition = req.body.workPosition;
+    const interestArea = req.body.interestArea;
+    const selfDescription = req.body.selfDescription;
+    let notification = req.body.notification;
 
-        console.log('id:', id);
-// console.log('userName:', userName);
-        const [rows, fields] = await db.execute('SELECT * FROM users WHERE id = ? OR userName = ? OR workPosition = ? OR interestArea = ? OR selfDescription = ? OR createdAt = ? OR updatedAt = ? OR googleId = ?', [id, userName, workPosition, interestArea, selfDescription, createdAt, updatedAt, googleId]);
-        console.log(rows)
+    if (notification = 'notification') {
+        notification = 1;
+    } else {
+        notification = 0;
+    };
+
+    try {
+        const [rows, fields] = await db.execute('SELECT * FROM users WHERE id = ? OR userName = ? OR workPosition = ? OR interestArea = ? OR selfDescription = ? OR googleId = ?', [id, userName, workPosition, interestArea, selfDescription, googleId]);
 
         if (rows.length > 0) {
-            res.send('실패')
+            await db.execute(`UPDATE users SET id = ?, userName = ?, workPosition = ?, interestArea = ?, selfDescription = ?, grade = 5, createdAt = now(), updatedAt = now(), notification = ? WHERE googleId = ?`, [id, userName, workPosition, interestArea, selfDescription, notification, googleId]);
+            res.send('업데이트 성공');
             // res.redirect('http://localhost:5173');
 
             // res.status(400).json({ message: '이미 가입된 회원입니다' });
         } else {
             // await db.execute(`INSERT INTO users (id, userName, workPosition, interestArea, selfDescription, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?)`, [id, userName, workPosition, interestArea, selfDescription, createdAt, updatedAt]);
-            await db.execute(`UPDATE users SET (id = ?, userName = ?, workPosition = ?, interestArea = ?, selfDescription = ?, createdAt = ?, updatedAt = ?, grade = ?, notification = ?) WHERE googleId = ?`, [id, userName, workPosition, interestArea, selfDescription, createdAt, updatedAt]);
+            
             // res.redirect('http://localhost:5173');
-            res.send('성공')
+            res.send('실패');
             // res.json({ message: '회원가입 완료. 추가 정보를 입력하세요.' });
         }
     } catch (error) {
