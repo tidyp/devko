5118648afe7b8736a57e012b3042b18fce7869be
JoonHa Park <tidyp1030@icloud.com>
2024-01-02 17:04:59 +0900

    Revert "[Test] google test"

--- client/src/pages/exploer/Post.jsx
+++ client/src/pages/exploer/Post.jsx
@@ -1,37 +1,35 @@
 import styles from "./Post.module.scss";
 
-import { Link } from "react-router-dom";
-
-import { formatDate } from "../../utils/utils";
+import {formatDate} from '../../utils/utils'
 
 const Post = ({ post }) => {
-  const data = formatDate(post.WriteDate);
-  const page = `/${post.cate}/${post.Id}`;
+  const data = formatDate(post.WriteDate)
+
 
   return (
     <>
-      <Link to={page}>
-        <div className={styles.container}>
-          {/* post header */}
-          <header className={styles.header}>
-            <div className={styles.profileImage}>
-              <img src={post.profileImage} alt="" />
-            </div>
+      <div className={styles.container}>
+        {/* post header */}
+        <header className={styles.header}>
+          <div className={styles.profileImage}>
+            <img src={post.profileImage} alt="" />
+          </div>
 
-            <div className={styles.top}>
-              <p className={styles.title}>{post.Title}</p>
-              <p className={styles.bottom}>
-                <span className={styles.name}>{post.name}</span>
-                <span className={styles.date}>{data}</span>
-              </p>
-            </div>
-          </header>
-          {/* post 본문 */}
-          <main>{post.Content}</main>
-          {/* post footer */}
-          <footer>조회수 댓글갯수 좋아요 댓글 공유</footer>
-        </div>
-      </Link>
+          <div className={styles.top}>
+            <p className={styles.title}>{post.Title}</p>
+            <p className={styles.bottom}>
+              <span className={styles.name}>{post.name}</span>
+              <span className={styles.date}>{data}</span>
+            </p>
+          </div>
+        </header>
+        {/* post 본문 */}
+        <main>{post.Content}</main>
+        {/* post footer */}
+        <footer>
+          조회수 댓글갯수 좋아요 댓글 공유
+        </footer>
+      </div>
     </>
   );
 };
