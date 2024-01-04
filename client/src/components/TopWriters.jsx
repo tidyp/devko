4dbe19702d73f8db85d9f07336b33cdd060c7dbc
tidyp <tidyp1030@icloud.com>
2024-01-04 16:23:31 +0900

    [Mod] express

---
+++ client/src/components/TopWriters.jsx
@@ -0,0 +1,40 @@
+const TopWriters = () => {
+  return (
+    <div className="box-border flex h-auto w-auto flex-col items-center justify-center    gap-3.5  rounded-2xl  bg-slate-50 p-8 ">
+      <div className="text-base font-semibold text-black">TopWriters</div>
+      <div className="flex h-auto flex-col  gap-5 self-stretch px-2.5">
+        <div className="flex flex-row">
+          <div>
+            <p>사진</p>
+          </div>
+          <div>
+            <p>텍스트1</p>
+            <p>텍스트2</p>
+          </div>
+        </div>
+
+        <div className="flex flex-row">
+          <div>
+            <p>사진</p>
+          </div>
+          <div>
+            <p>텍스트1</p>
+            <p>텍스트2</p>
+          </div>
+        </div>
+
+        <div className="flex flex-row">
+          <div>
+            <p>사진</p>
+          </div>
+          <div>
+            <p>텍스트1</p>
+            <p>텍스트2</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopWriters;
