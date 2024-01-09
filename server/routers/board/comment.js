75ffafb9551fa3be9f76f5a17f01b8cd92ff7c2e
iamjaeeuncho <redjoun@gmail.com>
2024-01-09 23:33:22 +0900

    refactoring

--- server/routers/board/comment.js
+++ server/routers/board/comment.js
@@ -6,9 +6,8 @@ require("dotenv").config();
 
 // 댓글 목록 보기
 router.get("/:postId", async (req, res) => {
-  const postId = req.params.postId;
-
   try {
+    const postId = req.params.postId;
     const sql = `SELECT * FROM comments WHERE postId = ? ORDER BY postId, id, mainId, createdAt ASC`;
     const [rows, fields] = await db.query(sql, [postId]);
     res.send(rows);
@@ -53,8 +52,8 @@ router.post("/:postId?", async (req, res) => {
 });
 
 // 댓글 수정
-router.put("/edit/:commentId?", async (req, res) => {
-  const commentId = req.params.commentId;
+router.put("/:id?", async (req, res) => {
+  const commentId = req.params.id;
   const content = req.body.content;
   const updatedAt = new Date();
 
@@ -72,8 +71,8 @@ router.put("/edit/:commentId?", async (req, res) => {
 });
 
 // 댓글 삭제
-router.delete("/delete/:commentId?", async (req, res) => {
-  const commentId = req.params.commentId;
+router.delete("/:id?", async (req, res) => {
+  const commentId = req.params.id;
   const sql = "DELETE FROM comments WHERE id = ?";
 
   try {
@@ -86,33 +85,22 @@ router.delete("/delete/:commentId?", async (req, res) => {
 });
 
 // 대댓글 작성
-router.post("/subcomment/:postId?", async (req, res) => {
+router.post("/sub/:id?", async (req, res) => {
   try {
-    const postId = req.params.postId;
-    const { userId, content } = req.body;
-    const createdAt = new Date();
-    const updatedAt = new Date();
+    const commentId = req.params.id;
+    const { userId, postId, content } = req.body;
 
-    const sql =
-      "INSERT INTO comments (userId, postId, content, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?)";
-    const result = await db.query(sql, [
+    const insertSql = `INSERT INTO comments (userId, postId, mainId, content, createdAt, updatedAt) VALUES (?, ?, ?, ?, NOW(), NOW())`;
+    const selectSql = `SELECT * FROM comments WHERE userId = ? AND postId = ? AND mainId = ? ORDER BY createdAt DESC LIMIT 1`;
+
+    await db.query(insertSql, [userId, postId, commentId, content]);
+    const [rows, fields] = await db.query(selectSql, [
       userId,
       postId,
-      content,
-      createdAt,
-      updatedAt,
+      commentId,
     ]);
 
-    const newComment = {
-      id: result.insertId,
-      userId,
-      postId,
-      content,
-      createdAt,
-      updatedAt,
-    };
-
-    res.status(201).json(newComment);
+    res.send(rows);
   } catch (err) {
     console.error("Error creating comment: " + err.stack);
     res.status(500).json({ error: "Internal Server Error" });
