6fb3f441579f565a6b3bea10543bade61a86eb3d
6worry <dudfhr217@naver.com>
2024-01-17 16:15:47 +0900

    프로필 조회 및 이메일 찾기 일부 완료

--- server/routers/user/profile.js
+++ server/routers/user/profile.js
@@ -5,7 +5,7 @@ const db = require("../../config/db");
 require("dotenv").config();
 
 // 프로필 페이지
-router.get("/:id", async (req, res) => {
+router.get("/:userName", async (req, res) => {
   try {
     const sql = `
     SELECT u.userName AS userName,
@@ -13,36 +13,38 @@ router.get("/:id", async (req, res) => {
            u.workPosition AS workPosition,
            u.interestArea AS interestArea,
            u.selfDescription AS selfDescription,
-           u.grade AS grade,
-           p.userId AS postID,
-           p.category AS category,
-           p.title AS title,
-           p.content AS postContent,
-           c.userId As commentID,
-           c.content AS commentContent
+           u.grade AS grade
     FROM users u
-    JOIN posts p ON p.userId = u.id
-    JOIN comments c ON c.userId = u.id
-    WHERE u.id = ?
-    `;
-    const userId = req.body.userId;
-    // const userId = 'ca436c51-f3b7-45fe-9a7e-275269a81e6e';
+    WHERE u.userName = ?
+           `;
+          //  p.userId AS postID,
+          //  p.category AS category,
+          //  p.title AS title,
+          //  p.content AS postContent,
+          //  c.userId As commentID,
+          //  c.content AS commentContent
+    // JOIN posts p ON p.userId = u.id
+    // JOIN comments c ON c.userId = u.id
+    // const userId = req.body.userId;
+    const userId = 'd';
 
     const [rows, fields] = await db.query(sql, userId);
-    console.log(fields)
-    // if (rows.length > 0) {
-    //   await db.execute(
-    //     `UPDATE users SET userName = ?, workPosition = ?, interestArea = ?, selfDescription = ?, grade = 5, createdAt = now(), updatedAt = now(), notification = ? WHERE id = ?`,
-    //     [
-    //       userName,
-    //       workPosition,
-    //       interestArea,
-    //       selfDescription,
-    //       notification,
-    //       userId,
-    //     ]
-    //   );
-    // }
+    console.log(rows)
+    rows.forEach(row => {
+      if (row.grade = 5) {
+        row.grade = 'junior'
+      } else if (row.grade = 4) {
+        row.grade = 'middle'
+      } else if (row.grade = 3) {
+        row.grade = 'senior'
+      } else if (row.grade = 2) {
+        row.grade = 'CTO'
+      } else if (row.grade = 1) {
+        row.grade = 'CEO'
+      } else if (row.grade = 0) {
+        row.grade = 'admin'
+      };
+    });
     res.json(rows);
   } catch (err) {
     console.error("Query execution error:", err);
@@ -67,13 +69,15 @@ router.get("/image", async (req, res) => {
 
 // 이메일 찾기 -- 이전 필요 (/find/info)
 
-router.get("/info", async (req, res) => {
-  const sql = `SELECT ug.googleEmail, un.naverEmail FROM users_google ug
-  JOIN users u on u.id = ug.id
-  JOIN users_naver un on un.id = u.id
-  WHERE ug.googleEmail = ? or un.naverEmail`;
-  const googleEmail = req.body.googleEmail;
-  const naverEmail = req.body.naverEmail;
+router.get("/find/info", async (req, res) => {
+  const sql = `SELECT un.naverEmail , ug.googleEmail FROM usersnaver un
+  JOIN users u ON u.naverId = un.id
+  LEFT join usersgoogle ug ON ug.id = u.googleId
+  WHERE un.naverEmail = ? OR ug.googleEmail = ?`;
+  // const googleEmail = req.body.googleEmail;
+  const googleEmail = 'jinyr5654@gmail.com';
+  // const naverEmail = req.body.naverEmail;
+  const naverEmail = 'dudfhr217@jr.naver.com';
 
   try {
     const [rows, fields] = await db.query(sql, [googleEmail, naverEmail]);
