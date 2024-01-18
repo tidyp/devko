37fe239795c3cc77dceb6130158a764831567fe3
iamjaeeuncho <redjoun@gmail.com>
2024-01-18 21:14:44 +0900

    add daily stat queries

--- server/routers/board/dailystat.js
+++ server/routers/board/dailystat.js
@@ -3,6 +3,44 @@ const router = express.Router();
 const db = require("../../config/db");
 
 // Popular Tags
-router.get("/tag", async (req, res) => {});
-const sql = ``;
+router.get("/tag", async (req, res) => {
+  try {
+    const sql = `
+          SELECT name AS tagName
+              , count(*) AS tagCNT
+          FROM tags
+          GROUP BY name
+          ORDER BY count(*) DESC
+          `;
+
+    const [rows, fields] = await db.query(sql);
+    res.send(rows);
+  } catch (err) {
+    console.error("Query execution error:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Top Users
+router.get("/user", async (req, res) => {
+  try {
+    const sql = `
+        SELECT u.id AS userId
+            , COUNT(p.id) AS postCNT
+            , COUNT(c.id) AS commentCNT
+        FROM users u
+        LEFT OUTER JOIN posts p ON u.id = p.userId
+        LEFT OUTER JOIN comments c ON u.id = c.userId
+        GROUP BY u.id, c.id
+        ORDER BY COUNT(p.id), COUNT(c.id) DESC
+          `;
+
+    const [rows, fields] = await db.query(sql);
+    res.send(rows);
+  } catch (err) {
+    console.error("Query execution error:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
