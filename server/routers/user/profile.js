5507eab05e5c581732a6025a3e8458cc1a150a22
6worry <dudfhr217@naver.com>
2024-01-24 04:10:13 +0900

    프로필 점수 구현

--- server/routers/user/profile.js
+++ server/routers/user/profile.js
@@ -96,12 +96,11 @@ router.get("/image", async (req, res) => {
 
 // 커뮤니티 포인트 구현
 
-router.get("/point", async (req, res) => {
-  // t.count AS teamCnt,
-  // SUM(p.count, c.count, t.count) AS TotalCnt -- gpt 하기
+router.get("/:userId/point", async (req, res) => {
   const sql = `
   SELECT p.count AS postCnt,
-         c.count AS commentCnt,
+        c.count AS commentCnt,
+        pt.count AS teamCnt
   FROM (
       SELECT u.id AS id
         , u.userName AS userName
@@ -113,31 +112,45 @@ router.get("/point", async (req, res) => {
         , un.naverId AS naverId
         , un.naverEmail AS naverEmail
         , un.naverImage AS naverImage
-  FROM users u
-  LEFT OUTER JOIN usersgoogle ug ON u.googleId = ug.id
-  LEFT OUTER JOIN usersnaver un ON u.naverId = un.id) u
+      FROM users u
+      LEFT OUTER JOIN usersgoogle ug ON u.googleId = ug.id
+      LEFT OUTER JOIN usersnaver un ON u.naverId = un.id) u
   LEFT OUTER JOIN (SELECT userId, COUNT(*) AS count FROM posts GROUP BY userId) p ON u.id = p.userId
+  LEFT OUTER JOIN (SELECT userId, COUNT(*) AS count FROM posts WHERE posts.category = 'group' GROUP BY userId) pt ON u.id = pt.userId
   LEFT OUTER JOIN (SELECT userId, COUNT(*) AS count FROM comments GROUP BY userId) c ON u.id = c.userId
   WHERE u.id =?
   `;
   // const userId = req.body.userId;
-  const userId = 'ca436c51-f3b7-45fe-9a7e-275269a81e6e';
+  const userId = 'd77faaa9-b197-4d8f-b897-eae3a4cd9b71';
 
   try {
     const [rows, fields] = await db.query(sql, [userId]);
-    // for (let n=0, n<length, n++) { -- gpt 하기
-    //   for (let j=0, j<length, j++) {
-    //     rows.postCnt = 1/3 * n;
-    //     rows.commentCnt = 1/5 * j;
+    console.log(rows);
 
-    //     if (rows.postCnt > 3000) {
-    //       rows.postCnt = 1000; 
-    //     } else if (rows.commentCnt > 5000) {
-    //       rows.commentCnt = 1000;
-    //     }
-    //   }
-    // }
-    res.json(rows);
+    let postPoint = Math.floor((1/3 * rows[0].postCnt) * 100) / 100;
+    let commentPoint = Math.floor((1/5 * rows[0].commentCnt) * 100) / 100;
+    const teamPoint = Math.floor((rows[0].teamCnt) * 100) / 100;
+    
+    if (postPoint - Math.floor(1/3 * rows[0].postCnt) < 0.25 ||
+        commentPoint - Math.floor(1/5 * rows[0].commentCnt) < 0.25
+    ) {
+      postPoint = Math.floor(postPoint);
+      commentPoint = Math.floor(commentPoint);
+    } else if (postPoint - Math.floor(1/3 * rows[0].postCnt) >= 0.25 && 
+               postPoint - Math.floor(1/3 * rows[0].postCnt) < 0.75 ||
+               commentPoint - Math.floor(1/5 * rows[0].commentCnt) >= 0.25 &&
+               commentPoint - Math.floor(1/5 * rows[0].commentCnt) < 0.75
+    ) {
+      postPoint = Math.floor(postPoint) + 0.5;
+      commentPoint = Math.floor(commentPoint) + 0.5;
+    } else {
+      postPoint = Math.ceil(postPoint);
+      commentPoint = Math.ceil(commentPoint);
+    };
+
+    const totalPoint = postPoint + commentPoint + teamPoint;
+
+    res.json({postPoint, commentPoint, teamPoint, totalPoint});
   } catch (err) {
     console.error("Query execution error:", err);
     res.status(500).send("Internal Server Error");
