8ceb3fd4cce97df862b81ad682dfb95ab3ed9e2e
tidyp <tidyp1030@icloud.com>
2024-01-23 18:24:00 +0900

    fixed error

--- server/routers/user/profile.js
+++ server/routers/user/profile.js
@@ -6,52 +6,57 @@ require("dotenv").config();
 
 // 프로필 페이지
 router.get("/:userId", async (req, res) => {
-  const userId = req.params.userId;
-  const userinfo = `SELECT * FROM users WHERE id = ?`;
-  const userpost = `SELECT * FROM posts WHERE userId = ?`;
-  const usercomment = `SELECT * FROM comments WHERE userId = ?`;
-
   try {
-    const [userinfoData] = await db.execute(userinfo, [`${userId}`]);
-    const [userpostData] = await db.execute(userpost, [`${userId}`]);
-    const [usercommentData] = await db.execute(usercomment, [`${userId}`]);
-    // const sql = `
-    // SELECT u.userName AS userName,
-    //        u.profileImage AS profileImage,
-    //        u.workPosition AS workPosition,
-    //        u.interestArea AS interestArea,
-    //        u.selfDescription AS selfDescription,
-    //        u.grade AS grade,
-    //        p.userId AS postID,
-    //        p.category AS category,
-    //        p.title AS title,
-    //        p.content AS postContent,
-    //        c.userId As commentID,
-    //        c.content AS commentContent
-    // FROM users u
-    // LEFT JOIN posts p ON p.userId = u.id
-    // LEFT JOIN comments c ON c.userId = u.id
-    // WHERE u.id = ?
-    // `;
+    const usersql = `
+    SELECT u.userName AS userName,
+           u.profileImage AS profileImage,
+           u.workPosition AS workPosition,
+           u.interestArea AS interestArea,
+           u.selfDescription AS selfDescription,
+           u.grade AS grade
+    FROM users u
+    WHERE u.id = ?
+    `;
 
-    // const [rows, fields] = await db.query(sql, [userId]);
-    // console.log(rows);
-    // rows.forEach((row) => {
-    //   if ((row.grade = 5)) {
-    //     row.grade = "junior";
-    //   } else if ((row.grade = 4)) {
-    //     row.grade = "middle";
-    //   } else if ((row.grade = 3)) {
-    //     row.grade = "senior";
-    //   } else if ((row.grade = 2)) {
-    //     row.grade = "CTO";
-    //   } else if ((row.grade = 1)) {
-    //     row.grade = "CEO";
-    //   } else if ((row.grade = 0)) {
-    //     row.grade = "admin";
-    //   }
-    // });
-    res.json({ userinfoData, userpostData, usercommentData });
+    const postsql = `
+    Select p.userId AS postID,
+           p.category AS category,
+           p.title AS title,
+           p.content AS postContent
+    FROM users u
+    LEFT JOIN posts p ON p.userId = u.id
+    WHERE u.id = ?
+    `;
+
+    const commentsql = `
+    Select c.userId As commentID,
+           c.content AS commentContent
+    FROM users u
+    LEFT JOIN comments c ON c.userId = u.id
+    WHERE u.id = ?
+    `;
+
+    const userId = req.params.userId;
+
+    const [userrows, userfields] = await db.query(usersql, [userId]);
+    userrows.forEach(row => {
+      if (row.grade = 5) {
+        row.grade = 'junior'
+      } else if (row.grade = 4) {
+        row.grade = 'middle'
+      } else if (row.grade = 3) {
+        row.grade = 'senior'
+      } else if (row.grade = 2) {
+        row.grade = 'CTO'
+      } else if (row.grade = 1) {
+        row.grade = 'CEO'
+      } else if (row.grade = 0) {
+        row.grade = 'admin'
+      };
+    });
+    const [postrows, postfields] = await db.query(postsql, [userId]);
+    const [commentrows, commentfields] = await db.query(commentsql, [userId]);
+    res.json({userrows, postrows, commentrows});
   } catch (err) {
     console.error("Query execution error:", err);
     res.status(500).send("Internal Server Error");
@@ -60,32 +65,32 @@ router.get("/:userId", async (req, res) => {
 
 // 프로필 이미지 수정 구현
 router.get("/image", async (req, res) => {
-  const sql = `SELECT profileImage FROM users u WHERE u.id = ?`;
-  // const userId = req.body.userId;
-  const userId = "ca436c51-f3b7-45fe-9a7e-275269a81e6e";
+    const sql = `SELECT profileImage FROM users u WHERE u.id = ?`;
+    // const userId = req.body.userId;
+    const userId = 'ca436c51-f3b7-45fe-9a7e-275269a81e6e';
 
-  try {
-    const [rows, fields] = await db.query(sql, [userId]);
-    rows.forEach((row) => {
-      if ((row.grade = 5)) {
-        row.grade = "junior";
-      } else if ((row.grade = 4)) {
-        row.grade = "middle";
-      } else if ((row.grade = 3)) {
-        row.grade = "senior";
-      } else if ((row.grade = 2)) {
-        row.grade = "CTO";
-      } else if ((row.grade = 1)) {
-        row.grade = "CEO";
-      } else if ((row.grade = 0)) {
-        row.grade = "admin";
-      }
-    });
-    res.send(rows[0].profileImage);
-  } catch (err) {
-    console.error("Query execution error:", err);
-    res.status(500).send("Internal Server Error");
-  }
+    try {
+      const [rows, fields] = await db.query(sql, [userId]);
+      rows.forEach(row => {
+        if (row.grade = 5) {
+          row.grade = 'junior'
+        } else if (row.grade = 4) {
+          row.grade = 'middle'
+        } else if (row.grade = 3) {
+          row.grade = 'senior'
+        } else if (row.grade = 2) {
+          row.grade = 'CTO'
+        } else if (row.grade = 1) {
+          row.grade = 'CEO'
+        } else if (row.grade = 0) {
+          row.grade = 'admin'
+        };
+      });
+      res.send(rows[0].profileImage);
+    } catch (err) {
+      console.error("Query execution error:", err);
+      res.status(500).send("Internal Server Error");
+    };
 });
 
 // 커뮤니티 포인트 구현
@@ -115,7 +120,7 @@ LEFT OUTER JOIN (SELECT userId, COUNT(*) AS count FROM comments GROUP BY userId)
 WHERE u.id =?
 `;
   // const userId = req.body.userId;
-  const userId = "ca436c51-f3b7-45fe-9a7e-275269a81e6e";
+  const userId = 'ca436c51-f3b7-45fe-9a7e-275269a81e6e';
 
   try {
     const [rows, fields] = await db.query(sql, [userId]);
@@ -125,7 +130,7 @@ WHERE u.id =?
     //     rows.commentCnt = 1/5 * j;
 
     //     if (rows.postCnt > 3000) {
-    //       rows.postCnt = 1000;
+    //       rows.postCnt = 1000; 
     //     } else if (rows.commentCnt > 5000) {
     //       rows.commentCnt = 1000;
     //     }
@@ -135,31 +140,7 @@ WHERE u.id =?
   } catch (err) {
     console.error("Query execution error:", err);
     res.status(500).send("Internal Server Error");
-  }
-});
-
-// 이메일 찾기 -- 이전 필요 (/find/info)
-
-router.get("/find/info", async (req, res) => {
-  const sql = `
-  SELECT un.naverEmail ,
-         ug.googleEmail
-  FROM usersnaver un
-  LEFT JOIN users u ON u.naverId = un.id
-  LEFT JOIN usersgoogle ug ON ug.id = u.googleId
-  WHERE un.naverEmail = ? OR ug.googleEmail = ?`;
-  // const googleEmail = req.body.googleEmail;
-  const googleEmail = "jinyr5654@gmail.com";
-  // const naverEmail = req.body.naverEmail;
-  const naverEmail = "dudfhr217@jr.naver.com";
-
-  try {
-    const [rows, fields] = await db.query(sql, [googleEmail, naverEmail]);
-    res.json(rows);
-  } catch (err) {
-    console.error("Query execution error:", err);
-    res.status(500).send("Internal Server Error");
-  }
+  };
 });
 
 module.exports = router;
