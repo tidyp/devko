218c2c726420da70cba2326498a660543a688641
iamjaeeuncho <redjoun@gmail.com>
2024-01-28 11:00:41 +0900

    modify daily stats

--- server/routers/board/daily.js
+++ server/routers/board/daily.js
@@ -4,18 +4,22 @@ const db = require("../../config/db");
 const cron = require("node-cron");
 
 router.get("/tag", (req, res) => {
-  let result = [];
-  // 매일 자정에 실행되는 스케줄러 설정
-  // cron.schedule("0 0 * * *", () => {
-  cron.schedule("*/1 * * * *", () => {
-    console.log("데이터 스케쥴러", new Date());
+  const fetchData = async () => {
+    try {
+      console.log("데이터 스케쥴러", new Date());
 
-    const sql = "SELECT * FROM posts";
+      const sql = "SELECT * FROM posts LIMIT 5";
 
-    result = db.query(sql);
-    console.log(result);
-  });
-  res.send(result);
+      const result = await db.query(sql);
+      res.send(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  };
+
+  cron.schedule("*/10 * * * *", fetchData); // 10초
+  // cron.schedule("0 0 * * *", fetchData); // 자정
 });
 
 module.exports = router;
