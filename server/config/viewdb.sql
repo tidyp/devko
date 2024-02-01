e80e30f2cf6c54e2c638509dcaa3166a393a18e5
iamjaeeuncho <redjoun@gmail.com>
2024-02-01 16:46:18 +0900

    typo

--- server/config/viewdb.sql
+++ server/config/viewdb.sql
@@ -50,14 +50,13 @@ SELECT b.id AS postId
 	, u.profileImage AS profileImage
 	, u.grade AS grade
     , c.count AS commentCnt
-    , t.id AS tagId
     , t.name AS tagName
     , l.count AS likeCnt
     , v.count AS viewCnt
 FROM boardsView b
 LEFT OUTER JOIN usersView u ON b.userId = u.id
 LEFT OUTER JOIN (SELECT category, postId, COUNT(id) AS count FROM comments GROUP BY category, postId) c ON b.id = c.postId AND b.category = c.category
-LEFT OUTER JOIN (SELECT category, postId, GROUP_CONCAT(name) AS name FROM devko.tags GROUP BY category, postId) t ON b.id = t.postId AND b.category = t.category
+LEFT OUTER JOIN (SELECT category, postId, GROUP_CONCAT(name) AS name FROM tags GROUP BY category, postId) t ON b.id = t.postId AND b.category = t.category
 LEFT OUTER JOIN (SELECT category, postId, SUM(count) AS count FROM likes GROUP BY category, postId) l ON b.id = l.postId AND b.category = l.category
 LEFT OUTER JOIN views v ON b.id = v.postId AND b.category = v.category
 ORDER BY b.createdAt ASC
