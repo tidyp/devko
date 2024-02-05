a0bf306c8bb5c9ea4930f49e99f952e57e257987
6worry <dudfhr217@naver.com>
2024-02-05 16:15:44 +0900

    refactoring codes

--- server/routers/board/search.js
+++ server/routers/board/search.js
@@ -7,27 +7,28 @@ router.get("/:input", async (req, res) => {
   try {
     const input = req.params.input;
     const sql = `
-    SELECT bv.id AS id
-        , bv.category AS category
-        , bv.title AS title
-        , bv.content AS content
-        , bv.createdAt AS createdAt
-        , bv.updatedAt AS updatedAt
-        , uv.id AS userId
-        , uv.userName AS userName
-        , uv.profileImage AS profileImage
-        , uv.grade AS grade
-    FROM boardsView bv
-    LEFT OUTER JOIN usersView uv ON bv.userId = uv.id
-    WHERE uv.userName LIKE ? OR bv.title LIKE ? OR bv.content LIKE ?
+      SELECT bv.id AS id
+           , bv.category AS category
+           , bv.title AS title
+           , bv.content AS content
+           , bv.createdAt AS createdAt
+           , bv.updatedAt AS updatedAt
+           , uv.id AS userId
+           , uv.userName AS userName
+           , uv.profileImage AS profileImage
+           , uv.grade AS grade
+      FROM boardsView bv
+      LEFT OUTER JOIN usersView uv ON bv.userId = uv.id
+      WHERE uv.userName LIKE ? OR bv.title LIKE ? OR bv.content LIKE ?
     `;
 
     const [rows, fields] = await db.query(sql, [
-      `%${input}%`,
-      `%${input}%`,
-      `%${input}%`,
+      `%${input}%`, // uv.userName
+      `%${input}%`, // or bv.title
+      `%${input}%` // or bv.content
     ]);
 
+    // 검색 페이징 구현
     const itemsPerPage = 10;
     const page = parseInt(req.params.page) || 1;
 
@@ -37,11 +38,7 @@ router.get("/:input", async (req, res) => {
     const currPageRows = rows.slice(startIndex, endIndex);
     const totalPages = Math.ceil(rows.length / itemsPerPage);
 
-    res.json({
-      currPageRows,
-      totalPages,
-      page,
-    });
+    res.json({ currPageRows, totalPages, page });
   } catch (err) {
     console.error("Query execution error:", err);
     res.status(500).send("Internal Server Error");
--- server/routers/user/user.js
+++ server/routers/user/user.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const multer = require('multer');
-const db = require('../../config/db');
-const fs = require('fs');
-const {v4: uuidv4} = require('uuid');
-require('dotenv').config();
+const path = require("path");
+const multer = require("multer");
+const db = require("../../config/db");
+const fs = require("fs");
+const {v4: uuidv4} = require("uuid");
+require("dotenv").config();
 
 // 파일 업로드 폴더 정의 및 정의된 폴더 없을시 자동생성
 const uploadPath = ("public/src/profileimages/");
@@ -19,8 +19,8 @@ const upload = multer({
   dest: uploadPath,
   limits: { fileSize: 10 * 512 * 512 }, // 10mb 제한
   fileFilter: (req, file, cb) => {
-    if (!file.mimetype.startsWith('image/')) {
-        return cb(new Error('사진 파일만 첨부하세요.'), false);
+    if (!file.mimetype.startsWith("image/")) {
+        return cb(new Error("사진 파일만 첨부하세요."), false);
     };
 
     cb(null, true);
@@ -28,7 +28,7 @@ const upload = multer({
 });
 
 // - 최초 회원가입시 정보 post 여기다 하기 (예정)
-router.post('/', upload.single('profileImage'), async (req, res) => {
+router.post("/", upload.single("profileImage"), async (req, res) => {
   const userId = uuidv4();
   const userName = req.body.userName;
   const profileImage = req.body.googleImage || req.body.naverImage;
@@ -68,7 +68,7 @@ router.post('/', upload.single('profileImage'), async (req, res) => {
 });
 
 // 내 정보 조회
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const sql = `SELECT * FROM users u WHERE u.id = ?`;
   const userId = req.params.id;
 
@@ -78,16 +78,16 @@ router.get('/:id', async (req, res) => {
     res.json({ users: row });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   };
 });
 
 // 내 정보 수정
-router.put('/:id', upload.single('profileImage'), async (req, res) => {
+router.put("/:id", upload.single("profileImage"), async (req, res) => {
   const userId = req.params.id;
   const { userName, interestPosition, interestArea, selfDescription } = req.body;
 
-  if (req.body.path && typeof req.body.path === 'string') {
+  if (req.body.path && typeof req.body.path === "string") {
     const oldProfileImage = path.join(__dirname, "public", "src", "profileimages", req.body.path);
 
     // 이전 프로필파일 제거
@@ -98,28 +98,28 @@ router.put('/:id', upload.single('profileImage'), async (req, res) => {
 
   const profileImage = req.file ? req.file.path : null;
   const updateSql = profileImage
-    ? 'UPDATE users SET userName = ?, profileImage = ?, interestPosition = ?, interestArea = ?, selfDescription = ?, updatedAt = NOW() WHERE id = ?'
-    : 'UPDATE users SET userName = ?, interestPosition = ?, interestArea = ?, selfDescription = ?, updatedAt = NOW() WHERE id = ?';
+    ? "UPDATE users SET userName = ?, profileImage = ?, interestPosition = ?, interestArea = ?, selfDescription = ?, updatedAt = NOW() WHERE id = ?"
+    : "UPDATE users SET userName = ?, interestPosition = ?, interestArea = ?, selfDescription = ?, updatedAt = NOW() WHERE id = ?";
 
   const updateParams = profileImage
     ? [{userName}, profileImage, interestPosition, interestArea, selfDescription, userId]
     : [userName, interestPosition, interestArea, selfDescription, userId];
 
   await db.query(updateSql, updateParams);
-  res.redirect('/');
+  res.redirect("/");
 });
 
 // 회원 탈퇴
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   const userId = req.params.id;
 
-  db.query('DELETE FROM users WHERE id = ?', [userId], (err) => {
+  db.query("DELETE FROM users WHERE id = ?", [userId], (err) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: "Internal Server Error" });
     };
 
-    res.status(200).json({ message: 'User deleted successfully' });
+    res.status(200).json({ message: "User deleted successfully" });
   });
 });
 
