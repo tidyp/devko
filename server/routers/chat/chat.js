eef8dc174583872f5a03e9375428b8062995d83c
6worry <dudfhr217@naver.com>
2024-02-05 17:39:14 +0900

    refactoring codes

--- server/routers/chat/chat.js
+++ server/routers/chat/chat.js
@@ -4,22 +4,18 @@ const WebSocket = require('ws');
 const router = express.Router();
 const path = require('path');
 
-const port = 3001;
-
-const app = express();
-expressWs(app);
-
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'chat.html'));
 });
 
-const wsClient = new Map(); // 접속자 정보를 저장하기 위한 자료구조
+// 접속자 정보를 저장하기 위한 자료구조
+const wsClient = new Map();
 
 // 웹소켓 핸들링 코드
-
 router.ws('/chat', (ws ,req) => {
+    // 소켓 연결 요청온 것에 대하여 처리함
     const clientIp = req.socket.remoteAddress;
-    console.log('클라이언트 접속함', clientIp); // 소켓 연결 요청온 것에 대하여 처리함
+    console.log('클라이언트 접속함', clientIp);
         
     ws.on('message', (message) => {
         console.log(message.toString()); // 연결된 이후 내부 메세지 처리하는 부분 
@@ -29,7 +25,8 @@ router.ws('/chat', (ws ,req) => {
         let username;
 
         try {
-            parsedMessage = JSON.parse(message); // 받은 문자열 파싱하여 객체 형태로 만듬
+            // 받은 문자열 파싱하여 객체 형태로 만듬
+            parsedMessage = JSON.parse(message);
             messageType = parsedMessage.type;
             username = parsedMessage.username;
 
@@ -49,7 +46,9 @@ router.ws('/chat', (ws ,req) => {
         if (messageType !== 'session') {
             wsClient.forEach((client) => {
                 console.log('username:', username);
-                if (client.readyState === WebSocket.OPEN) { // 모든 클라이언트에게 메세지 재전송
+
+                // 모든 클라이언트에게 메세지 재전송
+                if (client.readyState === WebSocket.OPEN) {
                     const messageType = client === ws ? 'sent' : 'received';
                     const messageObj = { type: messageType, sender: username, content: parsedMessage.content};
                 
@@ -58,7 +57,6 @@ router.ws('/chat', (ws ,req) => {
             });
         };
     });
-        
         
     ws.on('close', () => {
         // 연결된 이후 연결 종료를 처리하는 부분
--- server/routers/user/naverAuth.js
+++ server/routers/user/naverAuth.js
@@ -11,6 +11,7 @@ let state = "RANDOM_STATE";
 const redirectURI = encodeURI("http://localhost:3000/api/naverAuth/callback");
 let api_url = "";
 
+// 회원가입
 router.get("/login", function (req, res) {
   api_url =
     "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
@@ -46,12 +47,13 @@ router.get("/callback", async (req, res) => {
     url: api_url,
     headers: {
       "X-Naver-Client-Id": client_id,
-      "X-Naver-Client-Secret": client_secret,
-    },
+      "X-Naver-Client-Secret": client_secret
+    }
   };
   const result = await request.get(options);
   const token = JSON.parse(result).access_token;
 
+  // 사용자의 네이버 계정 정보
   const info_options = {
     url: "https://openapi.naver.com/v1/nid/me",
     headers: { Authorization: "Bearer " + token },
@@ -60,6 +62,7 @@ router.get("/callback", async (req, res) => {
   const info_result = await request.get(info_options);
   const info_result_json = JSON.parse(info_result).response;
 
+  // 기존 회원 여부 확인 및 신규 회원 가입
   const naverId = info_result_json.id;
   const naverEmail = info_result_json.email;
   const naverImage = info_result_json.profile_image;
@@ -75,10 +78,13 @@ router.get("/callback", async (req, res) => {
         SELECT * FROM usersView uv WHERE uv.naverId = ?
       `;
       const [rows, field] = await db.query(userSql, [naverId]);
+      
       res.cookie("uuid", rows[0].id, { secure: true });
       res.cookie("userName", rows[0].userName, { secure: true });
       res.cookie("userImage", rows[0].profileImage, { secure: true });
       res.redirect("http://localhost:5173");
+
+    // 없는 회원, 신규 회원가입 + 추가 정보 입력
     } else {
       await db.execute(
         "INSERT INTO usersnaver (naverId, naverEmail, naverImage) VALUES (?, ?, ?)",
@@ -87,11 +93,11 @@ router.get("/callback", async (req, res) => {
       res.cookie("naverId", naverId, { secure: true });
       res.cookie("naverImage", naverImage, { secure: true });
       res.redirect("http://localhost:5173/addinfo");
-    }
+    };
   } catch (error) {
     console.error("Database query error: ", error);
     res.status(500).json({ message: "Internal server error" });
-  }
+  };
 });
 
 module.exports = router;
